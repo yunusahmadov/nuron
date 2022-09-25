c0d8286408f7f0c5b051d9de4ca1f3659177b9aa
Yunus Ahmadov <yunus.ahmedov.11@mail.ru>
2022-09-25 16:47:00 +0400

    adding responsive

--- jquery.js
+++ jquery.js
@@ -53,3 +53,23 @@
 //         $('.to_top').hide()
 //     }
 // });
+
+$( document ).ready(function() {
+
+    $( ".cross" ).hide();
+    $( ".menu" ).hide();
+    $( ".hamburger" ).click(function() {
+    $( ".menu" ).slideToggle( 300, function() {
+    $( ".hamburger" ).hide();
+    $( ".cross" ).show();
+    });
+    });
+    
+    $( ".cross" ).click(function() {
+    $( ".menu" ).slideToggle( 300, function() {
+    $( ".cross" ).hide();
+    $( ".hamburger" ).show();
+    });
+    });
+    
+    });
